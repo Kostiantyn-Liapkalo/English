511ebd8b08837354f83db5ae465c6b3452b641e6
Kostiantyn Liapkalo <97124993+Kostiantyn-Liapkalo@users.noreply.github.com>
2023-05-10 12:18:14 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+// script.js
+
+// Пример скрипта для обработки отправки формы контактов
+document.addEventListener('DOMContentLoaded', function() {
+  const form = document.querySelector('form');
+  form.addEventListener('submit', function(event) {
+    event.preventDefault();
+    // Ваш код обработки отправки формы здесь
+  });
+});
